3052d65b6d8512381538b5ecbb435f1e3886e4d6
sca4cs <shannon.atkinson.ca@gmail.com>
2018-09-14 12:09:40 -0400

    register and login functions

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,9 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
 
-const { authenticate } = require('./middlewares');
+const db = require('../database/dbConfig.js');
+
+const { authenticate, generateToken } = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,11 +12,44 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  // implement user registration
+  const creds = req.body;
+  const hash = bcrypt.hashSync(creds.password, 12);
+  creds.password = hash;
+  db('users')
+    .insert(creds)
+    .then(ids => {
+      const id = ids[0];
+      db('users')
+        .where({ id })
+        .first()
+        .then(user => {
+          const token = generateToken(user);
+          res.status(201).json({ id: user.id, username: user.username, token });
+        })
+        .catch(err => {
+          res.status(500).send(err)});
+    })
+    .catch(err => {
+      if (err.errno === 19) {
+        res.status(400).json({ error: "Please provide a unique username." })
+      } else
+      res.status(500).send(err)});
 }
 
 function login(req, res) {
-  // implement user login
+  const creds = req.body;
+  db('users')
+    .where({ username: creds.username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(creds.password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({ token });
+      } else {
+        res.status(401).json({ message: 'Invalid username or password.' });
+      }
+    })
+    .catch(err => res.status(500).send(err));
 }
 
 function getJokes(req, res) {
